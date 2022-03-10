4ef300dad70c7536ba54fce39e79baebfcbac7b8
bortoliana <analeticiadebortoli@gmail.com>
2022-03-10 19:22:57 -0300

    Domain model, database seed

--- backend/src/main/java/com/dsmovie/dsmovei/config/SecurityConfig.java
+++ backend/src/main/java/com/dsmovie/dsmovei/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.dsmovie.dsmovei.config;
 
-package com.devsuperior.dsmovie.config;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
