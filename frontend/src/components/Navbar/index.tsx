856902f003ce3142f48645c087fa61efc6675d99
bortoliana <analeticiadebortoli@gmail.com>
2022-03-12 16:43:44 -0300

    Pagination

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>AnaBortoli</h1>
+                    <h1>bortoliana</h1>
                     <a href="https://github.com/bortoliana" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,49 +1,51 @@
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import MovieCard from 'components/MovieCard';
 import Pagination from 'components/Pagination';
-import { useEffect, useState } from 'react';
-import { BASE_URL } from 'utils/requests';
+import { BASE_URL } from 'utils/request';
 import { MoviePage } from 'types/movie';
-
 
 function Listing() {
 
-    const [pageNumber, setPageNumber] = useState (0);
+    const [pageNumber, setPageNumber] = useState(0);
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true,
+    });
 
-    const [page, setPage] = useState<MoviePage> ({
-            content: [],
-            last: true,
-            totalPages: 0,
-            totalElements: 0.
-            size: 12,
-            number: 0,
-            first: true, 
-            numberOfElements: 0,
-            empty: true
-        });
+    useEffect(() => {
+      axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+      .then(response => {
+          const data = response.data as MoviePage;
+          setPage(data);
+      })
+    }, [pageNumber])
 
-    useEffect (() => {
-        axios.get('${BASE_URL}/movie')
-        .then(response => {
-           const data = response.data as MoviePage;
-           setPage(data);
-        });
-    },[pageNumber]);
- 
+    const handlePageChange = (newPageNumber: number) => {
+        setPageNumber(newPageNumber);
+    }
+
     return (
         <>
-            <Pagination/>
+        <Pagination page ={page} onChange={handlePageChange}/> 
 
             <div className="container">
                 <div className="row">
-                 {page.content.map(movie => (
-                     <div key ={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    {page.content.map(movie => (
+                        <div  key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard movie={movie} />
                     </div>
-                        )
-                        )}
-                    </div>
-                 </div>
+                    ))}
+                </div>
+            </div>
+
         </>
     )
 }
