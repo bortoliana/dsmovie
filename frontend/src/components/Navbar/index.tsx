e5579b93679369190a434efe7d90b262be914ac6
bortoliana <analeticiadebortoli@gmail.com>
2022-03-12 19:02:56 -0300

    ajuste rel html

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,11 +7,11 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>bortoliana</h1>
+                    <h1>DSMovie</h1>
                     <a href="https://github.com/bortoliana" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/bortoliana</p>
                         </div>
                     </a>
                 </div>
