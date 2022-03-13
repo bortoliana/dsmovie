3c1532357e2b796d47c485f59bd463b85b7b2b93
bortoliana <analeticiadebortoli@gmail.com>
2022-03-13 00:44:47 -0300

    criaçao de link DSMovie

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,9 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
+                    <h1>
+                        <a href="https://dsmovieana.netlify.app/" rel="noreferrer">DSMovie</a>
+                    </h1>
                     <a href="https://github.com/bortoliana" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
